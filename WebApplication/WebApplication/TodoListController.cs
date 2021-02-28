5242a86502fd8c939139476f20985b054da835cb
Laurence <laurence@accurx.com>
2021-02-28 17:48:32 +0000

    Add basic web server

---
+++ WebApplication/WebApplication/TodoListController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication
+{
+    [Route("api/[controller]")]
+    public class TodoListController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return Ok("Hello world 2");
+        }
+    }
+}
